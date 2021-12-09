9c6c08295893751b146f141e2b2e99ae247adada
kth0522 <rlaxogh369@gmai.com>
2021-12-09 15:16:24 +0900

    k means cluster aggregation

--- models/multi_view.py
+++ models/multi_view.py
@@ -12,8 +12,20 @@ from torch import nn
 from torch._six import inf
 from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
+from fast_pytorch_kmeans import KMeans
 
 
+def batch_kmeans(n_clusters, batch):
+    kmeans = KMeans(n_clusters=n_clusters, mode='cosine')
+    new_batch = []
+    for feature in batch:
+        new_feature = torch.zeros(n_clusters, feature.shape[1])
+        labels = kmeans.fit_predict(feature)
+        for i in range(labels.shape[0]):
+            new_feature[labels[i]] += feature[i].cpu()
+        new_batch.append(new_feature)
+
+    return torch.stack(new_batch).to(torch.cuda.current_device())
 
 class ViewMaxAgregate(nn.Module):
     def __init__(self,  model):
@@ -22,8 +34,13 @@ class ViewMaxAgregate(nn.Module):
 
     def forward(self, mvimages):
         B, M, C, H, W = mvimages.shape
-        pooled_view = torch.max(unbatch_tensor(self.model(batch_tensor(
-            mvimages, dim=1, squeeze=True)), B, dim=1, unsqueeze=True), dim=1)[0]
+
+        mvfeatures = unbatch_tensor(self.model(batch_tensor(
+            mvimages, dim=1, squeeze=True)), B, dim=1, unsqueeze=True)
+
+        c_mvfeatures = batch_kmeans(4, mvfeatures)
+
+        pooled_view = torch.max(c_mvfeatures, dim=1)[0]
         return pooled_view.squeeze()
 
 
